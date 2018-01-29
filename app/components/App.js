3c0f29121c8cf689924019b8f79aa3f6471d4fcb
Eddy Lu <eddy.lu@mail.mcgill.ca>
2018-01-28 19:29:44 -0800

    Set up project

---
+++ app/components/App.js
@@ -0,0 +1,10 @@
+var React = require('react');
+var ReactDom = require('react-dom');
+
+class App extends React.Component {
+    render() {
+        return <h1>Hello World!</h1>;
+    }
+}
+
+module.exports = App;
---
+++ app/index.js
@@ -0,0 +1,9 @@
+var React = require('react');
+var ReactDom = require('react-dom');
+
+var App = require('./components/App');
+
+ReactDom.render(
+	<App />,
+	document.getElementById('app')
+);

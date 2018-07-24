2de8b17764aa73be243286e03076133942a0b228
Eddy Lu <eddy.lu@mail.mcgill.ca>
2018-07-23 22:25:24 -0700

    Refactor logic in Message class
    Refactor Message styles into separate stylesheet

--- app/components/Message.js
+++ app/components/Message.js
@@ -1,72 +1,59 @@
 import React from 'react';
 
 import { formatDate } from '../util/Helpers';
+import messageStyles  from './MessageStyles';
 
 class Message extends React.Component {
-
-
     constructor(props) {
         super(props);
         this.state = {
-
         };
     }
 
-    renderMessage(){
-        var containerStyle = {
-            display: "flex", 
-            marginBottom: "10px"
-        };
-        var profilePictureStyle = {
-            width: "100px", 
-            height: "100px", 
-            borderRadius: "50%", 
+    renderProfilePicture() {
+        var profilePictureStyle = Object.assign({}, messageStyles.profilePictureStyle, {
             marginLeft: this.props.isCurrentUser ? "10px" : "0px"
+        });
 
-        };
-        var nameDateContainerStyle = {
-            marginLeft: "10px"
-        };
-        var messageBodyContainerStyle = {
-            display: "flex", 
-            flex: 1, 
-            marginLeft: "10px",
-            border: "1px solid #006600",
-            paddingLeft: "10px",
-            paddingRight: "10px",
+        return (
+            <img src="https://upload.wikimedia.org/wikipedia/commons/d/d6/Smiley_1741_Hennet.jpg" style={profilePictureStyle}/>
+        );
+    };
+
+    renderMessageBody() {
+        var messageBodyContainerStyle = Object.assign({}, messageStyles.messageBodyContainerStyle, {
             backgroundColor: this.props.isCurrentUser ? "#CCCCCC" :"#CEFFCE", 
-            borderRadius: "10px",
-            fontFamily: "OpenSans"
-        };
+        });
 
-
-        const { name, date, messageBody } = this.props;
-
-        return this.props.isCurrentUser ? (
-            <div style={containerStyle}>
-                <div style={messageBodyContainerStyle}>
-                    <h3>{messageBody}</h3>
-                </div>
-                <img src="https://upload.wikimedia.org/wikipedia/commons/d/d6/Smiley_1741_Hennet.jpg" style={profilePictureStyle}/>
-                <div style={nameDateContainerStyle}>
-                    <h3>{name}</h3>
-                    <h3>{formatDate(date)}</h3>
-                </div>
-                
-            </div>
-        ) : 
-        (
-            <div style={containerStyle}>
-                <img src="https://upload.wikimedia.org/wikipedia/commons/d/d6/Smiley_1741_Hennet.jpg" style={profilePictureStyle}/>
-                <div style={nameDateContainerStyle}>
-                    <h3>{name}</h3>
-                    <h3>{formatDate(date)}</h3>
-                </div>
-                <div style={messageBodyContainerStyle}>
-                    <h3>{messageBody}</h3>
-                </div>
+        return (
+            <div style={messageBodyContainerStyle}>
+                <h3>{this.props.messageBody}</h3>
             </div>
         );
+    };
+
+    renderNameDate() {
+        return (
+            <div style={messageStyles.nameDateContainerStyle}>
+                <h3>{this.props.name}</h3>
+                <h3>{formatDate(this.props.date)}</h3>
+            </div>
+        );
+    };
+
+    renderMessage(){
+        return this.props.isCurrentUser ?
+            <div style={messageStyles.containerStyle}>
+                {this.props.isCurrentUser}
+                {this.renderMessageBody()}
+                {this.renderProfilePicture()}
+                {this.renderNameDate()}
+            </div> :
+            <div>
+                {this.renderProfilePicture()}
+                {this.renderNameDate()}
+                {this.renderMessageBody()}
+            </div>;
     }
 
     render() {
---
+++ app/components/MessageStyles.js
@@ -0,0 +1,26 @@
+const messageStyles = {
+    containerStyle: {
+        display: "flex", 
+        marginBottom: "10px"
+    },
+    profilePictureStyle: {
+        width: "100px", 
+        height: "100px", 
+        borderRadius: "50%"
+    },
+    nameDateContainerStyle: {
+        marginLeft: "10px"
+    },
+    messageBodyContainerStyle: {
+        display: "flex", 
+        flex: 1, 
+        marginLeft: "10px",
+        border: "1px solid #006600",
+        paddingLeft: "10px",
+        paddingRight: "10px",
+        borderRadius: "10px",
+        fontFamily: "OpenSans"
+    }
+};
+
+export default messageStyles;

7022231caa85ae183a9229caaa50cceb389e0da5
aldonp-ms <alnaveap@microsoft.com>
2018-07-24 14:32:34 -0700

    Backend work

---
+++ backend/path.js
@@ -0,0 +1,89 @@
+var express = require('express');
+var app = express();
+
+var mongoClient = require('mongodb').MongoClient;
+var mongoUrl = "mongodb://localhost:27017/";
+var dbName = 'mentalhealthdb';
+
+
+// Collections
+var usersColl = "Users";
+var topicsColl = "Topics";
+
+
+app.set('port', process.env.PORT || 3000);
+
+
+app.post('/signup', function(postReq, postRes){
+	var obj = postReq.query;
+	var username = obj.username;
+
+	mongoClient.connect(mongoUrl, obj, function(connerErr, db) {
+		if (connerErr) throw connerErr;
+		var dbo = db.db(dbName);
+
+		// Verify if user already exists
+		dbo.collection(usersColl).find({username : username}).toArray(function(findErr, findRes) {
+			if (findErr) throw findErr;
+			if (findRes.length != 0) {
+				console.log("username already exists: ", username)
+				db.close();
+				postRes.json({statusMessage : -1});
+				return;
+			}
+
+			// Insert user into db
+			dbo.collection(usersColl).insertOne(obj, function(insertErr, insertRes) {
+				if (insertErr) throw insertErr;
+				console.log("user created: ", username);
+				db.close();
+				postRes.json({statusMessage : 1});
+			});
+		});
+	});
+});
+
+
+app.get('/login', function(postReq, postRes) {
+    var obj = postReq.query;
+
+	mongoClient.connect(mongoUrl, obj, function(connerErr, db) {
+		if (connerErr) throw connerErr;
+		var dbo = db.db(dbName);
+
+		// Verify if entry exists in users collection
+		dbo.collection(usersColl).find(obj).toArray(function(findErr, findRes) {
+			if (findErr) throw findErr;
+			if (findRes.length != 0) {
+				db.close();
+				postRes.json({statusMessage : 1});
+				return;
+			}
+
+			postRes.json({statusMessage : -1});
+		});
+	});
+});
+
+
+app.get('/gettopics', function(postReq, postRes) {
+	var obj = postReq.query;
+
+	mongoClient.connect(mongoUrl, obj, function(connerErr, db) {
+		if (connerErr) throw connerErr;
+		var dbo = db.db(dbName);
+
+		// Verify if entry exists in users collection
+		dbo.collection(topicsColl).find().toArray(function(findErr, findRes) {
+			if (findErr) throw findErr;
+			db.close();
+			postRes.json(findRes);
+		});
+	});
+});
+
+
+
+app.listen(app.get('port'), function(){
+    console.log('Listening...');
+})

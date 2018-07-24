bca684da6ba7b39ad8b33cf6965aaa224a08ab85
Eddy Lu <eddy.lu@mail.mcgill.ca>
2018-07-24 09:37:38 -0700

    Remove original Discussion component logic

--- app/components/DiscussionStyles.js
+++
@@ -1,9 +0,0 @@
-const DiscussionStyles = {
-    discussionTitleStyle: {
-        fontFamily: "Calibri",
-        textAlign: "center",
-        color: "#181818"
-    }
-};
-
-export default DiscussionStyles;

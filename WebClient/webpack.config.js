4655365f0f8bc4d17462f56f92d6c567a32af8bc
Amardeepsingh <amsiglan@microsoft.com>
2018-09-13 22:00:00 -0700

    cleaned up webpack-config a little

--- WebClient/webpack.config.js
+++ WebClient/webpack.config.js
@@ -59,16 +59,3 @@ module.exports = {
 	// 	console: true
 	// }
 };
-
-// module.exports = {
-
-//     module: {
-//         rules: [
-//             // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-//             { test: /\.(tsx?|jsx?)$/, loader: "awesome-typescript-loader" },
-
-//             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-//             { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
-//         ]
-//     }
-// };

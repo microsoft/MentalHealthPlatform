ecd5dd325936515b59bca2040564f1afc488726c
Simona Virga <svirga@purdue.edu>
2019-07-23 11:22:40 -0700

    latest news container

---
+++ client/src/Components/NewsPreview/news-preview.css.d.ts
@@ -0,0 +1,5 @@
+export const NewsDataImage: string;
+export const NewsDataTextContainer: string;
+export const NewsDataContainer: string;
+export const NewsDataTitle: string;
+export const NewsDataDate: string;
--- client/src/Components/NewsPreview/news-preview.tsx
+++ client/src/Components/NewsPreview/news-preview.tsx
@@ -2,11 +2,35 @@
 // Licensed under the MIT license.
 
 import * as React from 'react';
+import satya_nadella from './../../images/satya_nadella.jpg';
+import headspace from './../../images/headspace.png';
 
-// import * as classes from "./news-preview.css";
+import * as classes from "./news-preview.css";
 
 interface INewsPreviewProps {
 }
+
+interface INewsData {
+    date: string;
+    title: string;
+    description: string;
+    picture: string;
+}
+
+const newsData = [
+    {
+        date: "July 22, 2019",
+        title: "New headspace partnership!",
+        description: "Employees now get 20% off any Headspace membership. Come get your discount now!", 
+        picture: headspace
+    },
+    {
+        date: "July 21, 2019",
+        title: "Executive discussion",
+        description: "Satya Nadella sits down to discuss about the importance of mental health",
+        picture: satya_nadella
+    }
+];
 
 const NewsPreview = (props: INewsPreviewProps) => {
     const {
@@ -14,7 +38,16 @@ const NewsPreview = (props: INewsPreviewProps) => {
 
     return (
         <div>
-            Latest News
+            <h3>Latest News</h3>
+            <div className={classes.NewsDataContainer}>
+                <img src={newsData[0].picture} className={classes.NewsDataImage}/>
+                <div className={classes.NewsDataTextContainer}>
+                    <label className={classes.NewsDataTitle}>{newsData[0].title}</label>
+                    <label className={classes.NewsDataDate}>{newsData[0].date}</label>
+                    <label>{newsData[0].description}</label>
+                </div>
+            </div>
+           
         </div>
     );
 };

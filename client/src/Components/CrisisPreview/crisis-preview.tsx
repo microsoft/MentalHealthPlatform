727bc97266ede0660bee14ffa61862c8a1665f4a
Simona Virga <svirga@purdue.edu>
2019-07-23 13:18:39 -0700

    crisis component

--- client/src/Components/CrisisPreview/crisis-preview.tsx
+++ client/src/Components/CrisisPreview/crisis-preview.tsx
@@ -2,8 +2,10 @@
 // Licensed under the MIT license.
 
 import * as React from 'react';
+import phone from './../../images/phone.jpg';
+import online_support from './../../images/online_support.jpg';
 
-// import * as classes from "./crisis-preview.css";
+import * as classes from "./crisis-preview.css";
 
 interface ICrisisPreviewProps {
 }
@@ -13,8 +15,29 @@ const CrisisPreview = (props: ICrisisPreviewProps) => {
     } = props;
 
     return (
-        <div>
-            Crisis
+        <div className={classes.CrisisPreviewsOverallContainer}>
+            <div>
+                <label className={classes.CrisisPreviewsText}>
+                    Are you currently experiencing a crisis?
+                </label>
+            </div>
+            <div className={classes.CrisisPreviews}>
+                <div className={classes.CrisisPreviewsSupportContainer}>
+                    <div className={classes.CrisisPreviewSupportContainer}>
+                        <img src={phone} className={classes.CrisisPreviewImage}/>
+                        <label>Phone</label>
+                    </div>
+                    <div className={classes.CrisisPreviewSupportContainer}>
+                        <img src={online_support} className={classes.CrisisPreviewImage}/>
+                        <label>Online</label>
+                    </div>  
+                </div>
+                
+                <div className={classes.CrisisPreviewsButtonContainer}>
+                    <button className={classes.CrisisPreviewsButton}>Get help now</button>
+                </div>
+            </div>
+            
         </div>
     );
 };

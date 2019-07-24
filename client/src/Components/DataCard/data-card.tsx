5e38886ac49c0665a890340b720352f8498c9094
Joe Branger <jobrange@myaryn-dev01.redmond.corp.microsoft.com>
2019-07-24 14:28:35 -0700

    Add Therapists mockup

---
+++ client/src/Components/DataCard/data-card.tsx
@@ -0,0 +1,57 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+import * as React from "react";
+import { Link, match } from 'react-router-dom';
+import Icon from '../Icon/Icon';
+import * as classes from "./data-card.css";
+
+import { getShortenedTimeAndDate } from '../../util/Helpers';
+
+type DataCardDataType = {
+    url: string;
+    title: string;
+    subtitle?: string;
+    secondarySubtitle?: string;
+    rightText?: string;
+    rightSubText?: string;
+    rightImage?: string;
+    description: string;
+    footer?: string;
+};
+
+interface IDataCardProps {
+    key: string;
+    match: match<{}>;
+    data?: DataCardDataType;
+}
+
+const DataCard = (props: IDataCardProps): JSX.Element => {
+    const { data, match } = props;
+    let baseUrl = match.url;
+    if (baseUrl.charAt(baseUrl.length - 1) == '/') {
+        baseUrl = baseUrl.substring(0, baseUrl.length - 1);
+    }
+
+    return (
+        <Link className={classes.DataCardLinkContainer} to={`${baseUrl}/${data.url}`}>
+            <div className={classes.DataCardContainer}>
+                <div style={{ flex: 1 }}>
+                    <div className={classes.Title}>{data.title}</div>
+                    {data.subtitle && <div className={classes.Subtitle}>{data.subtitle}</div>}
+                    {data.secondarySubtitle && <div className={classes.SecondarySubtitle}>{data.secondarySubtitle}</div>}
+                    <div>{data.description}</div>
+                    {data.footer && <div className={classes.Footer}>{data.footer}</div>}
+                </div>
+                {data.rightText && <div style={{ marginLeft: 32, display: "flex", flexDirection: "row" }}>
+                    <div style={{ display: "flex", flexDirection: "column", marginLeft: 8 }}>
+                        <div className={classes.Subtitle}>{data.rightText}</div>
+                        {data.rightSubText && <div className={classes.Subtitle}>{data.rightSubText}</div>}
+                    </div>
+                </div>}
+            </div>
+        </Link >
+    )
+}
+
+export default DataCard;
--- client/src/Components/Therapists/therapists.tsx
+++ client/src/Components/Therapists/therapists.tsx
@@ -2,16 +2,86 @@
 // Licensed under the MIT license.
 
 import * as React from 'react';
+import DataCard from '../DataCard/data-card';
+import { withRouter, RouteComponentProps, match } from 'react-router-dom';
+import { baseGetRequest } from '../../util/base-requests';
 
 interface ITherapistsProps {
 }
 
-const Therapists = (props: ITherapistsProps) => {
-   return (
-        <div>
-            Therapists
-        </div>
-   );
+interface ITherapistData {
+    _id: string,
+    title: string,
+    subtitle?: string,
+    desc: string,
+    phone: string,
+    location: string
 }
 
-export default Therapists;
+interface ITherapistsProps { 
+    type: string
+}
+
+interface ITherapistsState {
+    therapistData: ITherapistData[],
+    loading: boolean
+}
+
+class Therapists extends React.Component<RouteComponentProps<ITherapistsProps>, ITherapistsState> {
+    constructor(props: RouteComponentProps<ITherapistsProps>) {
+        super(props);
+        this.state = {
+            therapistData: [],
+            loading: true
+        };
+    }
+
+    componentDidMount = () => {
+        this.retrieveTherapists();
+    }
+
+    retrieveTherapistsResponseHandler = (data: any) => {
+        this.setState({
+            therapistData: data.therapists,
+            loading: false
+        });
+    }
+
+    retrieveTherapistsErrorHandler = (error: any) => {
+        console.log(error);
+    }
+
+    retrieveTherapists = () => {
+        baseGetRequest("gettherapists", [], this.retrieveTherapistsResponseHandler, this.retrieveTherapistsErrorHandler);
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.therapistData.map(therapist => {
+                    return (
+                        <DataCard
+                        key={therapist._id}
+                        match={{
+                            url: 'http://www.google.com',
+                            params: {}, isExact: false,
+                            path: ''
+                        }}
+                        data={{
+                            url: '/pages/stuff',
+                            title: therapist.title,
+                            subtitle: therapist.subtitle,
+                            description: therapist.desc,
+                            rightText: therapist.phone,
+                            rightSubText: therapist.location,
+                            footer: 'Specializes in: Stress, Trauma, Anxiety'
+                        }}
+                    />
+                    );
+                })}
+            </div>
+        );
+    }
+}
+
+export default withRouter(Therapists);
